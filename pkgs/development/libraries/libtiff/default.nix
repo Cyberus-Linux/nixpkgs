d7c4cbbc559495e57a0d9d6bb9c3ecd0aaa2e6dc
Yarny0 <41838844+Yarny0@users.noreply.github.com>
2024-09-08 08:06:58 +0200

    libtiff: 4.6.0 -> 4.7.0
    Release notes: https://libtiff.gitlab.io/libtiff/releases/v4.7.0.html
    
    Fixed CVEs:
    * CVE-2023-52356 https://gitlab.com/libtiff/libtiff/-/issues/622
    * CVE-2024-7006 https://gitlab.com/libtiff/libtiff/-/issues/624
    
    This update incorporates the `lerc` dependency declaration,
    therefore we drop the corresponding patch, c.f.
    https://gitlab.com/libtiff/libtiff/-/merge_requests/633
    https://github.com/NixOS/nixpkgs/pull/320105
    
    (cherry picked from commit 919602500b571eea23fe580c62e9bad7a6d94447)

--- pkgs/development/libraries/libtiff/default.nix
+++ pkgs/development/libraries/libtiff/default.nix
@@ -28,13 +28,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "libtiff";
-  version = "4.6.0";
+  version = "4.7.0";
 
   src = fetchFromGitLab {
     owner = "libtiff";
     repo = "libtiff";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-qCg5qjsPPynCHIg0JsPJldwVdcYkI68zYmyNAKUCoyw=";
+    hash = "sha256-SuK9/a6OUAumEe1kz1itFJGKxJzbmHkBVLMnyXhIwmQ=";
   };
 
   patches = [
@@ -43,18 +43,6 @@ stdenv.mkDerivation (finalAttrs: {
     # libc++abi 11 has an `#include <version>`, this picks up files name
     # `version` in the project's include paths
     ./rename-version.patch
-    # https://gitlab.com/libtiff/libtiff/-/issues/622
-    (fetchpatch {
-      name = "CVE-2023-52356.patch";
-      url = "https://gitlab.com/libtiff/libtiff/-/commit/51558511bdbbcffdce534db21dbaf5d54b31638a.patch";
-      hash = "sha256-A1G23MEUS1AvoREcKFqoqV2sYtCqIMfzPaIIFpZNBWE=";
-    })
-    # https://gitlab.com/libtiff/libtiff/-/issues/624
-    (fetchpatch {
-      name = "CVE-2024-7006.patch";
-      url = "https://gitlab.com/libtiff/libtiff/-/commit/818fb8ce881cf839fbc710f6690aadb992aa0f9e.patch";
-      hash = "sha256-XbRQtNxbNMofKTbeTsbHBKv96KTKSGngCepWPIVWLH4=";
-    })
   ];
 
   postPatch = ''
