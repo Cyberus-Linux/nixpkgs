b52a23e554df3a699c95a09daa191975cea49c71
Noa Aarts <noa@voorwaarts.nl>
2024-10-30 19:27:08 +0100

    gnupg: 2.4.5 -> 2.4.7
    Changes: https://dev.gnupg.org/source/gnupg/browse/master/NEWS;gnupg-2.4.7
    (cherry picked from commit 563e111e67438cbe1609615ae3c639602f2e00a8)

--- pkgs/tools/security/gnupg/24.nix
+++ pkgs/tools/security/gnupg/24.nix
@@ -14,11 +14,11 @@ assert guiSupport -> enableMinimal == false;
 
 stdenv.mkDerivation rec {
   pname = "gnupg";
-  version = "2.4.5";
+  version = "2.4.7";
 
   src = fetchurl {
     url = "mirror://gnupg/gnupg/${pname}-${version}.tar.bz2";
-    hash = "sha256-9o99ddBssWNcM2002ESvl0NsP2TqFLy3yGl4L5b0Qnc=";
+    hash = "sha256-eyRwbk2n4OOwbKBoIxAnQB8jgQLEHJCWMTSdzDuF60Y=";
   };
 
   depsBuildBuild = [ buildPackages.stdenv.cc ];
@@ -45,6 +45,7 @@ stdenv.mkDerivation rec {
       sed -i 's,"libpcsclite\.so[^"]*","${lib.getLib pcsclite}/lib/libpcsclite.so",g' scd/scdaemon.c
     '';
 
+  NIX_CFLAGS_COMPILE = lib.optionalString stdenv.hostPlatform.isDarwin "-Wno-implicit-function-declaration";
   configureFlags = [
     "--sysconfdir=/etc"
     "--with-libgpg-error-prefix=${libgpg-error.dev}"
@@ -70,11 +71,6 @@ stdenv.mkDerivation rec {
 
     # Make libexec tools available in PATH
     for f in $out/libexec/*; do
-      if [[ "$(basename $f)" == "gpg-wks-client" ]]; then continue; fi
-      ln -s $f $out/bin/$(basename $f)
-    done
-
-    for f in $out/libexec/; do
       if [[ "$(basename $f)" == "gpg-wks-client" ]]; then continue; fi
       ln -s $f $out/bin/$(basename $f)
     done
