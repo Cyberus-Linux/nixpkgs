46aaacb074e16bb5cf130263df4ad32486d6cb59
Vladimír Čunát <v@cunat.cz>
2025-03-03 10:53:09 +0100

    gnutls: adapt linking after 3.8.8
    Upstream:
    ** build: More flexible library linking options for compression libraries, TPM, and liboqs support
       The configure options, --with-zstd, --with-brotli, --with-zlib,
       --with-tpm2, and --with-liboqs now take 4 states:
       yes/link/dlopen/no, to specify how the libraries are linked or
       loaded.
    
    I haven't figure out how to easily avoid ENABLE_DLOPEN from configure,
    but we don't have the other libraries so far, so it's simple anyway.
    
    (cherry picked from commit 41143fe5d693bf9630e0bf2bf57a4938bd47ad2d)

--- pkgs/development/libraries/gnutls/default.nix
+++ pkgs/development/libraries/gnutls/default.nix
@@ -76,15 +76,6 @@ stdenv.mkDerivation rec {
 
   patches = [
     ./nix-ssl-cert-file.patch
-    # Revert https://gitlab.com/gnutls/gnutls/-/merge_requests/1800
-    # dlopen isn't as easy in NixPkgs, as noticed in tests broken by this.
-    # Without getting the libs into RPATH they won't be found.
-    (fetchpatch2 {
-      name = "revert-dlopen-compression.patch";
-      url = "https://gitlab.com/gnutls/gnutls/-/commit/8584908d6b679cd4e7676de437117a793e18347c.diff";
-      revert = true;
-      hash = "sha256-r/+Gmwqy0Yc1LHL/PdPLXlErUBC5JxquLzCBAN3LuRM=";
-    })
   ];
 
   # Skip some tests:
@@ -114,6 +105,8 @@ stdenv.mkDerivation rec {
       (lib.enableFeature cxxBindings "cxx")
     ] ++ lib.optionals (stdenv.hostPlatform.isMinGW) [
       "--disable-doc"
+    ] ++ [ # do not dlopen in nixpkgs
+        "--with-zlib=link"
     ];
 
   enableParallelBuilding = true;
