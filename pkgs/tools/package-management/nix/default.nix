27a2ba4e079fe0213699b089ded34d6d652184aa
Samuel Dionne-Riel <samuel.dionne-riel@cyberus-technology.de>
2025-07-07 17:08:29 -0400

    nixVersions.nix_2_24: 2.24.14 -> 2.24.15
    NOT cherry picked from commit 3b3cecb403f975b5ae346b1a87a24cf973bc28eb
    See: https://github.com/NixOS/nixpkgs/pull/423312

--- pkgs/tools/package-management/nix/default.nix
+++ pkgs/tools/package-management/nix/default.nix
@@ -173,8 +173,8 @@ in lib.makeExtensible (self: ({
   };
 
   nix_2_24 = (common {
-    version = "2.24.14";
-    hash = "sha256-SthMCsj6POjawLnJq9+lj/UzObX9skaeN1UGmMZiwTY=";
+    version = "2.24.15";
+    hash = "sha256-GHqFHLxvRID2IEPUwIfRMp8epYQMFcvG9ogLzfWRbPc=";
     self_attribute_name = "nix_2_24";
   }).override (lib.optionalAttrs (stdenv.isDarwin && stdenv.isx86_64) {
     # Fix the following error with the default x86_64-darwin SDK:
