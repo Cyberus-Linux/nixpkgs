1cbbabb43c115e9d42d45364a0c5ab7547abe594
Silvan Mosberger <silvan.mosberger@moduscreate.com>
2025-07-22 19:29:07 +0200

    gnupg: Add patch comments
    (cherry picked from commit 12003e5b3a7fe65a1e070a52fbd9515effefe174)

--- pkgs/tools/security/gnupg/24.nix
+++ pkgs/tools/security/gnupg/24.nix
@@ -69,6 +69,7 @@ stdenv.mkDerivation rec {
   ++ lib.optionals withTpm2Tss [ tpm2-tss ];
 
   patches = [
+    # Without this, scdaemon isn't linked to libusb, causing smartcards to not work correctly
     ./fix-libusb-include-path.patch
     ./tests-add-test-cases-for-import-without-uid.patch
     ./accept-subkeys-with-a-good-revocation-but-no-self-sig.patch
@@ -78,6 +79,13 @@ stdenv.mkDerivation rec {
     ./v3-0001-Disallow-compressed-signatures-and-certificates.patch
   ];
 
+  # Switch the default key server to keys.openpgp.org
+  # The original motivation in 2019 was to switch away from the then-default SKS network: https://github.com/NixOS/nixpkgs/pull/63952
+  # In 2021 upstream also switched away, but to keyserver.ubuntu.com: https://dev.gnupg.org/rG47c4e3e00a7ef55f954c14b3c237496e54a853c1,
+  # while NixOS kept the keys.openpgp.org default: https://github.com/NixOS/nixpkgs/pull/159604
+  # TODO: Should this patch be removed so that the now-uncompromised default is used once again?
+  # A significant difference between the two seems to be that keys.openpgp.org is verifying keys, while keyserver.ubuntu.com isn't: https://unix.stackexchange.com/a/694528
+  # The keys.openpgp.org also has a great FAQ: https://keys.openpgp.org/about/faq
   postPatch = ''
     sed -i 's,\(hkps\|https\)://keyserver.ubuntu.com,hkps://keys.openpgp.org,g' configure configure.ac doc/dirmngr.texi doc/gnupg.info-1
   ''
