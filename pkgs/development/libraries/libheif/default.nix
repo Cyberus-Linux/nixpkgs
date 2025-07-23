37da106f187b56ec313105130eff9f41d2d371f0
Samuel Dionne-Riel <samuel.dionne-riel@cyberus-technology.de>
2025-07-23 13:58:08 -0400

    libheif: Revert “format all inactive Nix files”
    This makes it impossible to trivially cherry-pick, as that file was not
    reformatted at the same cadence.

--- pkgs/development/libraries/libheif/default.nix
+++ pkgs/development/libraries/libheif/default.nix
@@ -1,36 +1,30 @@
-{
-  lib,
-  stdenv,
-  fetchFromGitHub,
-  cmake,
-  pkg-config,
-  dav1d,
-  rav1e,
-  libde265,
-  x265,
-  libpng,
-  libjpeg,
-  libaom,
-  gdk-pixbuf,
+{ lib
+, stdenv
+, fetchFromGitHub
+, cmake
+, pkg-config
+, dav1d
+, rav1e
+, libde265
+, x265
+, libpng
+, libjpeg
+, libaom
+, gdk-pixbuf
 
-  # for passthru.tests
-  gimp,
-  imagemagick,
-  imlib2Full,
-  imv,
-  vips,
+# for passthru.tests
+, gimp
+, imagemagick
+, imlib2Full
+, imv
+, vips
 }:
 
 stdenv.mkDerivation rec {
   pname = "libheif";
   version = "1.17.6";
 
-  outputs = [
-    "bin"
-    "out"
-    "dev"
-    "man"
-  ];
+  outputs = [ "bin" "out" "dev" "man" ];
 
   src = fetchFromGitHub {
     owner = "strukturag";
@@ -65,13 +59,7 @@ stdenv.mkDerivation rec {
   '';
 
   passthru.tests = {
-    inherit
-      gimp
-      imagemagick
-      imlib2Full
-      imv
-      vips
-      ;
+    inherit gimp imagemagick imlib2Full imv vips;
   };
 
   meta = {
