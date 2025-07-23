2e93b7818235fc134460dc8154bceeb245168643
Samuel Dionne-Riel <samuel.dionne-riel@cyberus-technology.de>
2025-07-23 15:32:06 -0400

    jbig2dec: Handle CVE-2023-46361
    Debian-Tracker: https://security-tracker.debian.org/tracker/CVE-2023-46361

--- pkgs/development/libraries/jbig2dec/default.nix
+++ pkgs/development/libraries/jbig2dec/default.nix
@@ -2,6 +2,7 @@
   lib,
   stdenv,
   fetchurl,
+  fetchpatch,
   python3,
   autoconf,
   automake,
@@ -16,6 +17,15 @@ stdenv.mkDerivation rec {
     url = "https://github.com/ArtifexSoftware/jbig2dec/archive/${version}/jbig2dec-${version}.tar.gz";
     hash = "sha256-qXBTaaZjOrpTJpNFDsgCxWI5fhuCRmLegJ7ekvZ6/yE=";
   };
+
+  patches = [
+    # jbig2dec: Avoid uninitialized allocator in command-line tool.
+    (fetchpatch {
+      name = "CVE-2023-46361.patch";
+      url = "https://github.com/ArtifexSoftware/jbig2dec/commit/ee53a7e4bc7819d32e8c0b2057885bcc97586bf3.patch";
+      hash = "sha256-EbXoyoJglhdwUH+uG9SmKYPt/V0MXg84POW9RV9HHdI=";
+    })
+  ];
 
   postPatch = ''
     patchShebangs test_jbig2dec.py
