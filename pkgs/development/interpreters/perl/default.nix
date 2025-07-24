bb9b0b364255aaae963c904b9e9037e0ac971701
Samuel Dionne-Riel <samuel.dionne-riel@cyberus-technology.de>
2025-07-24 15:41:10 -0400

    perl536: Mark insecure
    It is used AFAICT into three (unsupported) packages. And one of them has
    it as an optional component, turned off by default.

--- pkgs/development/interpreters/perl/default.nix
+++ pkgs/development/interpreters/perl/default.nix
@@ -55,12 +55,18 @@ let
 
 in rec {
   # Maint version
-  perl536 = callPackage ./interpreter.nix {
+  perl536 = (callPackage ./interpreter.nix {
     self = perl536;
     version = "5.36.3";
     sha256 = "sha256-8qGtiBFjkaF2Ji3ULfxS7yKvtA9MDpgQ8V1WHm8ccmo=";
     inherit passthruFun;
-  };
+  }).overrideAttrs({ meta ? {}, ... }: {
+    meta = meta // {
+      knownVulnerabilities = (meta.knownVulnerabilities or []) ++ [
+        "CVE-2024-56406"
+      ];
+    };
+  });
 
   # Maint version
   perl538 = callPackage ./interpreter.nix {
