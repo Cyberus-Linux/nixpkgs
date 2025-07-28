79320b9f94b5d92c5d2a11b93b4bab660b2f5a88
Samuel Dionne-Riel <samuel.dionne-riel@cyberus-technology.de>
2025-07-28 11:04:10 -0400

    linux_5_4: Drop due to lack of SLTS

--- pkgs/top-level/linux-kernels.nix
+++ pkgs/top-level/linux-kernels.nix
@@ -63,15 +63,6 @@ in {
     # New vendor kernels should go to nixos-hardware instead.
     # e.g. https://github.com/NixOS/nixos-hardware/tree/master/microsoft/surface/kernel
 
-    linux_5_4 = callPackage ../os-specific/linux/kernel/mainline.nix {
-      branch = "5.4";
-      kernelPatches = [
-        kernelPatches.bridge_stp_helper
-        kernelPatches.request_key_helper
-        kernelPatches.rtl8761b_support
-      ];
-    };
-
     linux_5_10 = callPackage ../os-specific/linux/kernel/mainline.nix {
       branch = "5.10";
       kernelPatches = [
@@ -483,7 +474,6 @@ in {
 
   vanillaPackages = {
     # recurse to build modules for the kernels
-    linux_5_4 = recurseIntoAttrs (packagesFor kernels.linux_5_4);
     linux_5_10 = recurseIntoAttrs (packagesFor kernels.linux_5_10);
     linux_5_15 = recurseIntoAttrs (packagesFor kernels.linux_5_15);
     linux_6_1 = recurseIntoAttrs (packagesFor kernels.linux_6_1);
@@ -495,6 +485,7 @@ in {
     linux_4_9 = throw "linux 4.9 was removed because it will reach its end of life within 22.11"; # Added 2022-11-08
     linux_4_14 = throw "linux 4.14 was removed because it will reach its end of life within 23.11"; # Added 2023-10-11
     linux_4_19 = builtins.throw "linux 4.19 has been removed due to lack of support"; # CTRL-OS 2025-07-28
+    linux_5_4 = builtins.throw "linux 5.4 has been removed due to lack of SLTS support"; # CTRL-OS 2025-07-28
     linux_5_18 = throw "linux 5.18 was removed because it reached its end of life upstream"; # Added 2022-09-17
     linux_5_19 = throw "linux 5.19 was removed because it reached its end of life upstream"; # Added 2022-11-01
     linux_6_0 = throw "linux 6.0 was removed because it reached its end of life upstream"; # Added 2023-01-20
