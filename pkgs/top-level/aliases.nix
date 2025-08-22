78af9fed4ae85c76a7f3edcad6c614ecc4127a0d
Samuel Dionne-Riel <samuel.dionne-riel@cyberus-technology.de>
2025-08-22 09:35:07 -0400

    linux-kernels: 6.15 -> 6.16
    Still not supported officially, but used to stay on top of necessary
    changes.

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -711,6 +711,7 @@ mapAliases ({
   linuxPackages_6_11 = linuxKernel.packages.linux_6_11;
   linuxPackages_6_12 = linuxKernel.packages.linux_6_12;
   linuxPackages_6_15 = linuxKernel.packages.linux_6_15;
+  linuxPackages_6_16 = linuxKernel.packages.linux_6_16;
   linuxPackages_rpi0 = linuxKernel.packages.linux_rpi1;
   linuxPackages_rpi02w = linuxKernel.packages.linux_rpi3;
   linuxPackages_rpi1 = linuxKernel.packages.linux_rpi1;
@@ -743,6 +744,7 @@ mapAliases ({
   linux_6_11 = linuxKernel.packages.linux_6_11.kernel;
   linux_6_12 = linuxKernel.packages.linux_6_12.kernel;
   linux_6_15 = linuxKernel.packages.linux_6_15.kernel;
+  linux_6_16 = linuxKernel.packages.linux_6_16.kernel;
   linux_rpi0 = linuxKernel.packages.linux_rpi1.kernel;
   linux_rpi02w = linuxKernel.packages.linux_rpi3.kernel;
   linux_rpi1 = linuxKernel.packages.linux_rpi1.kernel;
