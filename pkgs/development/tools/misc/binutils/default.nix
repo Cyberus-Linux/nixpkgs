e38c4c8778e4d1e221bc15cc1b0cc8b01151f98d
Tyler Langlois <tjl@byu.net>
2025-10-06 14:35:12 -0600

    binutils: fix CVE-2025-5245
    Backport derived from upstream commit; see:
    https://nvd.nist.gov/vuln/detail/CVE-2025-5245
    
    (cherry picked from commit 88ceb91bdfeab0552e6a57c578a4d286a7c49ecd)

--- pkgs/development/tools/misc/binutils/default.nix
+++ pkgs/development/tools/misc/binutils/default.nix
@@ -102,6 +102,10 @@ stdenv.mkDerivation (finalAttrs: {
       # Backported against CVE patched in the 2.45 series. See:
       # https://nvd.nist.gov/vuln/detail/CVE-2025-5244
       ./CVE-2025-5244.diff
+
+      # Backported against CVE patched in the 2.45 series. See:
+      # https://nvd.nist.gov/vuln/detail/CVE-2025-5245
+      ./CVE-2025-5245.diff
     ]
     ++ lib.optional targetPlatform.isiOS ./support-ios.patch
     # Adds AVR-specific options to "size" for compatibility with Atmel's downstream distribution
