dd89f425cd058eb2d0607fafea8c4ec59239380f
Jon Hermansen <jon@jh86.org>
2025-10-06 22:51:51 +0000

    imagemagick: 7.1.2-4 -> 7.1.2-5
    (cherry picked from commit 8d3793dea975e6b46f053ae2c33c02e40a8f53ee)

--- pkgs/applications/graphics/ImageMagick/default.nix
+++ pkgs/applications/graphics/ImageMagick/default.nix
@@ -51,13 +51,13 @@ in
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "imagemagick";
-  version = "7.1.2-4";
+  version = "7.1.2-5";
 
   src = fetchFromGitHub {
     owner = "ImageMagick";
     repo = "ImageMagick";
     rev = finalAttrs.version;
-    hash = "sha256-FOai96rtDWFQZ8vest0pDl6m2swQwXp7hVO2sZa1U2A=";
+    hash = "sha256-THStvhNWu4OYotzxksGJ6l6VeTVRNoJeKoRG1LaJhNI=";
   };
 
   outputs = [ "out" "dev" "doc" ]; # bin/ isn't really big
