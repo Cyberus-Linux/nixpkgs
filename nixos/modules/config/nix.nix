7da100ee79bd0947cc10ab54546678b46378d5b9
Samuel Dionne-Riel <samuel.dionne-riel@cyberus-technology.de>
2025-12-02 11:01:15 -0500

    nixos/nix: Make nix.settings line-oriented
    This will make the follow-up commit neater.

--- nixos/modules/config/nix.nix
+++ nixos/modules/config/nix.nix
@@ -375,8 +375,12 @@ in
   config = mkIf cfg.enable {
     environment.etc."nix/nix.conf".source = nixConf;
     nix.settings = {
-      trusted-public-keys = [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" ];
-      substituters = mkAfter [ "https://cache.nixos.org/" ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      ];
+      substituters = mkAfter [
+        "https://cache.nixos.org/"
+      ];
       system-features = mkDefault (
         [ "nixos-test" "benchmark" "big-parallel" "kvm" ] ++
         optionals (pkgs.stdenv.hostPlatform ? gcc.arch) (
