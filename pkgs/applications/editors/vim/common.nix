42b3716bebf880605d7de9725e85f0cb8255c933
Samuel Dionne-Riel <samuel.dionne-riel@cyberus-technology.de>
2026-04-01 18:24:47 -0400

    vim: 9.2.0261 -> 9.2.0280
    CVE-2026-34714
    CVE-2026-34982
    CVE-2026-35177

--- pkgs/applications/editors/vim/common.nix
+++ pkgs/applications/editors/vim/common.nix
@@ -1,6 +1,6 @@
 { lib, fetchFromGitHub }:
 rec {
-  version = "9.2.0261";
+  version = "9.2.0280";
 
   outputs = [
     "out"
@@ -11,7 +11,7 @@ rec {
     owner = "vim";
     repo = "vim";
     rev = "v${version}";
-    hash = "sha256-A2AS9y/NjEJctmyhWHgB+eVWKnagtwsEhWVrviro4hU=";
+    hash = "sha256-mkQQpflXLzBVDtgl5YHCj9we1F4bzhJiH8VVn8YoVQE=";
   };
 
   enableParallelBuilding = true;
