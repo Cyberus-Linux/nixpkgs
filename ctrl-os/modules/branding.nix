bd225e6eb9911062f7e63aa6b690567da4b34403
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2026-07-08 14:31:34 +0200

    ctrl-os: change default channel

--- ctrl-os/modules/branding.nix
+++ ctrl-os/modules/branding.nix
@@ -10,5 +10,18 @@ in
       distroName = "CTRL-OS";
       distroId = "ctrl-os";
     };
+
+    system.defaultChannel =
+      mkDistroOverride "https://channels.ctrl-os.com/channel/ctrlos-${config.system.nixos.release}.tar.xz";
+
+    nix.settings = {
+      trusted-public-keys = [
+        "ctrl-os:baPzGxj33zp/P+GAIJXsr8ss9Law+qEEFViX1+flbv8="
+      ];
+
+      substituters = [
+        "https://cache.ctrl-os.com/"
+      ];
+    };
   };
 }
